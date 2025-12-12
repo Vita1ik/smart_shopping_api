1de390d1cc3c97250f9b3c5274a4c29ad10a8728
vitalikpastukh <vitalik.p@matic.com>
2025-12-13 00:47:32 +0200

    Add cors

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem 'rswag-ui'
 gem 'rswag-specs'
 gem 'sass-rails'
 gem 'annotate'
+gem 'rack-cors'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,17 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # In development, you can allow all origins ('*')
+    # In production, replace '*' with your actual frontend URL (e.g., 'https://myapp.netlify.app')
+    origins '*'
+
+    resource '*',
+             headers: :any,
+             # CRITICAL: You must allow OPTIONS here
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             # If you send the Token in the header, you might need to expose it
+             expose: ['Authorization']
+  end
+end
